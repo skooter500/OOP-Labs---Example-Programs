b2da3e7aedec656b83dfabf3e5a6476d7e1465a7
Bryan Duggan <skooter500@gmail.com>
2013-10-29 13:41:46 +0000

    Bla

---
+++ SolutionToLabTest2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABCFiles2012
+{
+    class Program
+    {
+        static List<Tune> tunes = new List<Tune>();
+
+        static void LoadTunes(string filename)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filename);
+
+            bool hasTitle = false;
+            bool hasAltTitle = false;
+            int currentTune = 0;
+            Tune tune = null;
+
+            for (int i = 0; i < lines.Count; i ++)
+            {
+                string line = lines[i];
+                // Seach for the X: header. If we find one, its a new tune...
+                if (line.IndexOf("X:") != -1)
+                {
+                    tune = new Tune();
+                    tunes.Add(tune);
+                    hasTitle = false;
+                    hasAltTitle = false;
+                    tune.x = int.Parse(line.Substring(2));
+                }
+                if (line.IndexOf("K:") != -1)
+                {
+                    tune.keySig = line.Substring(2);
+                }
+                if (line.IndexOf("M:") != -1)
+                {
+                    tune.timeSig = line.Substring(2);
+                }
+                if (line.IndexOf("T:") != -1)
+                {
+                    if (hasTitle)
+                    {
+                        if (!hasAltTitle)
+                        {
+                            tune.altTitle = line.Substring(2);
+                            hasAltTitle = true;
+                        }
+                    }
+                    else
+                    {
+                        tune.title = line.Substring(2);
+                        hasTitle = true;
+                    }
+                }
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            string line;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader("hnr1.abc");
+            bool hasTitle = false;
+            bool hasAltTitle = false;
+            Tune[] tunes = new Tune[500];
+            int currentTune = 0;
+            Tune tune = null;
+            while ((line = file.ReadLine()) != null)
+            {                
+                // Seach for the X: header. If we find one, its a new tune...
+                if (line.IndexOf("X:") != -1)
+                {
+                    tune = new Tune();
+                    tunes[currentTune ++] = tune;
+                    hasTitle = false;
+                    hasAltTitle = false;
+                    tune.x = int.Parse(line.Substring(2));
+                }
+                if (line.IndexOf("K:") != -1)
+                {
+                    tune.keySig = line.Substring(2);
+                }
+                if (line.IndexOf("M:") != -1)
+                {
+                    tune.timeSig = line.Substring(2);
+                }
+                if (line.IndexOf("T:") != -1)
+                {
+                    if (hasTitle)
+                    {
+                        if (!hasAltTitle)
+                        {
+                            tune.altTitle = line.Substring(2);
+                            hasAltTitle = true;
+                        }                        
+                    }
+                    else
+                    {
+                        tune.title = line.Substring(2);
+                        hasTitle = true;
+                    }
+                }
+            }
+            Console.WriteLine("Found " + currentTune + " tunes");
+            for (int i = 0; i < currentTune; i++)
+            {
+                tunes[i].PrintHeaders();
+            }
+            Console.WriteLine("All done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SolutionToLabTest2/Tune.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABCFiles2012
+{
+    public class Tune
+    {
+        public int x;
+        public string title;
+        public string altTitle;
+        public string keySig;
+        public string timeSig;
+
+        public Tune()
+        {
+            x = 0;
+            title = "";
+            altTitle = "";
+            keySig = "";
+            timeSig = "";
+        }
+
+        public Tune(int x, string title)
+        {
+            this.x = x;
+            this.title = title;
+            altTitle = "";
+            keySig = "";
+            timeSig = "";
+        }
+
+        public void PrintHeaders()
+        {
+            Console.Write(x + "," + title);
+            if (altTitle != "")
+            {
+                Console.Write(", " + altTitle);
+            }
+            if (keySig != "")
+            {
+                Console.Write(", " + keySig);
+            }
+            if (timeSig != "")
+            {
+                Console.Write(", " + timeSig);
+            }
+            Console.WriteLine();
+        }
+    }
+}
