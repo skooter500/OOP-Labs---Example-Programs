7b1b97feeb073fa1700dad1658e9be688f095e1e
skooter500 <skooter500@gmail.com>
2014-03-05 15:15:04 +0000

    Added the BVector example from the class (Operator Overloading)

---
+++ BVector/ConsoleApplication2/BVector.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public struct BVector
+    {
+        private float x, y;
+
+        public BVector(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        #region Arithmetic Operators
+        public static BVector operator +(BVector a, BVector b)
+        {
+            BVector ret = new BVector();
+            ret.x = a.x + b.x;
+            ret.y = a.y + b.y;
+            return ret;
+        }
+
+        public static BVector operator -(BVector a)
+        {
+            BVector ret = new BVector();
+            ret.x = - a.x;
+            ret.y = - a.y;
+            return ret;
+        }
+
+        public static BVector operator -(BVector a, BVector b)
+        {
+            BVector ret = new BVector();
+            ret = a + (-b);
+            return ret;
+        }
+
+        public static BVector operator *(BVector a, float b)
+        {
+            BVector ret = new BVector();
+            ret.x = a.x * b;
+            ret.y = a.y * b;
+            return ret;
+        }
+
+        public static float operator *(BVector a, BVector b)
+        {
+            return (a.x * b.x) + (a.y * b.y);
+        }
+
+        public static BVector operator /(BVector a, float b)
+        {
+            return a * (1.0f / b);
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + " , " + y + ")";
+        }
+#endregion
+        #region Equality Operators
+        public static bool operator ==(BVector a, BVector b)
+        {
+            return (a.x == b.x) && (a.y == b.y);
+        }
+        public static bool operator !=(BVector a, BVector b)
+        {
+            return !(a == b);
+        }
+
+
+
+
+
+        public float Length
+        {
+            get
+            {
+                return (float) Math.Sqrt((x * x) + (y * y));
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ BVector/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BVector v1, v2, v3;
+
+            v1 = new BVector(5, 8);
+            v2 = new BVector(2, 5);
+
+            v3 = v1 + v2;
+            Console.WriteLine("v1: " + v1);
+            Console.WriteLine("v2: " + v2);
+            Console.WriteLine("v3: " + v3);
+
+            v1 += v2;
+
+            v1 = v2 - v3;
+
+            float f = 10.0f;
+            v1.X = 5;
+            v1.Y = 5;
+            v2 = v1 * f;
+            Console.WriteLine(v2);
+            Console.ReadLine();
+        }
+    }
+}
